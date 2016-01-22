c59598d38c9f2e0fc3b6812b01d22496b98aed74
dav <dvillaluenga@gmail.com>
2016-01-22 13:50:36 +0100

    PArece

---
+++ form_validate/cgi-bin/server.js
@@ -0,0 +1,98 @@
+//Lets require/import the HTTP module
+var http = require('http'),
+    fs = require('fs'),
+    dispatcher = require('httpdispatcher'),
+    mime = require('mime-types');
+//Lets define a port we want to listen to
+const PORT=8080;
+function randomString(numChar) {
+    return Array(numChar).fill(0).reduce(function (x ,y) {
+        return x + String.fromCharCode(97 + Math.floor(Math.random()*26));
+    }, "").toString();
+}
+function createData(content) {
+    var date = new Date(),
+        extension = ".json",
+        fileName = date.getTime() + randomString(5) + extension,
+        file;
+    file = fs.open("../cgi-bin/folder/" + fileName, 'w+', function(err) {
+        if(err) {
+            return console.log(err);
+        } else {
+            console.log("The file" + fileName + "was open!");
+        }
+    });
+    fs.writeFile("../cgi-bin/folder/" + fileName, content, function(err) {
+        if(err) {
+            return console.log(err);
+        } else {
+            console.log("The file was saved!");   
+        }
+    });
+}
+//We need a function which handles requests and send response
+function handleRequest(request, response){
+    try {
+        //log the request on console
+        console.log(request.url);
+        //Disptach
+        dispatcher.dispatch(request, response);
+    } catch(err) {
+        console.log(err);
+    }
+}
+function readData(req, res, type, file) {
+    console.log(type);
+    if(type === "text/css" || type === "image/gif" || type === "image/jpg" || type === "application/x-font-ttf") {
+        fs.stat(file, function (err, stat) {
+            var img = fs.readFileSync(file);
+            res.contentType = 'image/png';
+            res.contentLength = stat.size;
+            res.end(img, 'binary');
+        });
+    } else {
+        fs.readFile(file, function (err, data){
+            res.writeHead(200, {"Content-Type": type, 'Content-Length':data.length});
+            res.write(data);
+            res.end();
+        }); 
+    }
+}
+//For all your static (js/css/images/etc.) set the directory name (relative path).
+dispatcher.setStatic('../html');
+//A sample GET request    
+dispatcher.onGet("/", function(req, res) {
+    var date = new Date();
+    console.log("../html" + req.url);
+    fs.readFile("../html/index.html", function (err, data){
+        res.writeHead(200, 
+            {
+                "Content-Type": "text/html",
+                "Content-Length":data.length,
+                "Set-Cookie" : "Shield = " + date.getTime() + randomString(5)
+            });
+        res.write(data);
+        res.end();
+    });
+});
+//A sample POST request
+dispatcher.onGet(/.(css|js|jpg|png|gif|ttf)$/, function(req, res) { 
+    readData(req, res, mime.lookup(req.url), "../html" + req.url);
+});
+dispatcher.onPost("/registro", function(req, res) {
+    var StringRequest = JSON.stringify(req.params);
+    createData(StringRequest);
+    fs.readFile("../html/index.html", function (err, data){
+        res.writeHead(200, {"Content-Type": "text/html", "Content-Length":data.length, "Set-Cookie" : "Register_user=true"});
+        res.write(data);
+        res.end();
+    });
+});
+//Create a server
+var server = http.createServer(handleRequest);
+
+//Lets start our server
+server.listen(PORT, function(){
+    //Callback triggered when server is successfully listening. Hurray!
+    console.log("Server listening on: http://localhost:%s", PORT);
+});
--- form_validate/html/js/front-end.js
+++ form_validate/html/js/front-end.js
@@ -1,14 +1,36 @@
-/*jslint
-    node: true,
-    browser: true,
-    unparam: true
-*/
 /*globals
     globals,
     dealernet,
     document
 */
+/*jslint
+    node: true,
+    browser: true,
+    unparam: true
+*/
 'use strict';
+var globals = (function (ns) {
+    ns.ERR_LAST_NAME = "Debes introducir tu nombre y al menos un apellido";
+    ns.ERR_EMAIL = "Debes introducir un correo valido";
+    ns.ERR_PASS = "La contraseña debe tener al menos seis caracteres y una " +
+        "letra mayúscula, una letra minúscula, un número y un símbolo";
+    ns.ERR_CONF_PASS = "La confirmación y la contraseña deben coincidir";
+    ns.ERR_URL = "Debes introducir una URL válida";
+    ns.ERR_ADDRESS = "Debes introducir una dirección válida";
+    ns.ERR_COUNTRY = "Debes seleccionar un pais válido";
+    ns.ERR_POST_CODE = "Debes introducir un codigo postal válido";
+    ns.COUNTRIES = ["España", "Francia", "Portugal", "Italia", "Alemania"];
+    ns.SPAIN = ns.COUNTRIES[0];
+    ns.LAST_NAME_REG_EXP = /^[a-zñ\-\'\.]+(?:\s[a-zñ\-\'\.]+)+$/i;
+    ns.EMAIL_REG_EXP = /^[\w.]+@[\w.]+\.[\w]{2,6}$/i;
+    ns.PASS_REG_EXP = /^.*(?=.{6,})(?!.*\s)(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\!\@\#\$\%\^\&\*\(\)\-\=\¡\£\_\+\`\~\.\,\<\>\/\?\;\:\'\"\\\|\[\]\{\}]).*$/;
+    ns.URL_REG_EXP = /^[a-z]{2,}:([0-9]+|\/\/){1}[^\s-]*$/i;
+    ns.STANDARD_REG_EXP = /^.+$/;
+    ns.POST_CODE_REG_EXP = /^([0-4][0-9]{4}|[5][0-2][0-9]{3})$/;
+    ns.inputId = ["name", "email", "password", "confirm", "url", "address", "code"];
+    return ns;
+}({}));
+
 
 function $(id) {
     return document.getElementById(id);
@@ -47,32 +69,23 @@ function generateInput(type, name, identify) {
     input.setAttribute("class", identify);
     return input;
 }
-function ChooseCountry() {
-    var form_input = $("post_code"),
-        new_input;
-    if (this.value === globals.COUNTRIES[0]) {
-        form_input.appendChild(generateTextElement("label", "Codigo postal:"));
-        form_input.appendChild(generateInput("text", "post_code", ""));
+function chooseCountry() {
+    var form_input = $("post_code");
+    if (this.value === globals.SPAIN) {
         form_input.className = "form_input";
     } else {
-        if (form_input.childElementCount > 1) {
-            new_input = form_input.cloneNode(false);
-            new_input.className = "no_display";
-            form_input.remove();
-            $("form").insertBefore(new_input, $("form_comments"));
-        }
+        form_input.lastElementChild.value = "";
+        form_input.lastElementChild.className = "reset";
+        form_input.className = "no_display";
     }
 }
-function removeFirstWhite() {
-    this.removeChild(this.firstChild);
-    this.removeEventListener("change", removeFirstWhite, false);
-}
+
 function validateInput(regExp, input) {
     var checkClass;
     if (input === "") {
         checkClass = "reset";
     } else {
-        if(regExp.test(input)) {
+        if (regExp.test(input)) {
             checkClass = "valid";
         } else {
             checkClass = "invalid";
@@ -89,21 +102,72 @@ function validatePass() {
     var newClass = validateInput(globals.PASS_REG_EXP, this.value);
     this.className = newClass;
 }
+function validateEmail() {
+    var newClass = validateInput(globals.EMAIL_REG_EXP, this.value);
+    this.className = newClass;
+}
+function validateConfirm() {
+    if (this.value === "") {
+        this.className = "reset";
+    } else {
+        if (this.value === $("password").value) {
+            this.className = "valid";
+        } else {
+            this.className = "invalid";
+        }
+    }
+}
+function validateUrl() {
+    var newClass = validateInput(globals.URL_REG_EXP, this.value);
+    this.className = newClass;
+}
+function validateStandard() {
+    var newClass = validateInput(globals.STANDARD_REG_EXP, this.value);
+    this.className = newClass;
+}
+function validatePostCode() {
+    var newClass = validateInput(globals.POST_CODE_REG_EXP, this.value);
+    this.className = newClass;
+}
+function sendForm(e) {
+    var input; 
+    validateForm();
+    input = document.getElementsByClassName("invalid");
+    if (input.length === 0 && $("check_buttom").checked) {
+        document.forms[0].submit();
+    } else {
+        $("error").className = "form_input";
+    }
+}
+function validateForm() {
+    var inputs;
+    globals.inputId.forEach(function (element) {
+        $(element).focus();
+    });
+}
 window.onload = function () {
-    //var input_box = [$("name_error"), $("email_error"), $("pass_error"), $("confirm_error"), $("url_error"), $("address_error"), $("name_error"), $("country_error")]
     createError("name_error", globals.ERR_LAST_NAME);
     createError("email_error", globals.ERR_EMAIL);
     createError("pass_error", globals.ERR_PASS);
     createError("confirm_error", globals.ERR_CONF_PASS);
     createError("url_error", globals.ERR_URL);
     createError("address_error", globals.ERR_ADDRESS);
-    $("select_country").addEventListener("change", removeFirstWhite, false);
-    buildSelect("select_country", globals.COUNTRIES, "", ChooseCountry);
+    buildSelect("select_country", globals.COUNTRIES, "", chooseCountry);
     $("name").addEventListener("keyup", validateName, false);
+    $("name").addEventListener("focus", validateName, false);
     $("password").addEventListener("keyup", validatePass, false);
-    //$("name_error").addEventListener("keyup", validateInput, false);
-    //$("name_error").addEventListener("keyup", validateInput, false);
-    //$("name_error").addEventListener("keyup", validateInput, false);
-
+    $("password").addEventListener("focus", validatePass, false);
+    $("email").addEventListener("keyup", validateEmail, false);
+    $("email").addEventListener("focus", validateEmail, false);
+    $("confirm").addEventListener("keyup", validateConfirm, false);
+    $("confirm").addEventListener("focus", validateConfirm, false);
+    $("address").addEventListener("keyup", validateStandard, false);
+    $("address").addEventListener("focus", validateStandard, false);
+    $("url").addEventListener("keyup", validateUrl, false);
+    $("url").addEventListener("focus", validateUrl, false);
+    $("code").addEventListener("keyup", validatePostCode, false);
+    $("code").addEventListener("focus", validatePostCode, false);
+    $("send").addEventListener("click", sendForm, false);
+    validateForm();
 };
 
