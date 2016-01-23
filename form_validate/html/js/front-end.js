3d01833a859a43922967d0f6232e18f48087f3d8
dav <dvillaluenga@gmail.com>
2016-01-23 20:28:18 +0100

    Miraelfantasmico

--- form_validate/html/js/front-end.js
+++ form_validate/html/js/front-end.js
@@ -1,16 +1,15 @@
-/*globals
-    globals,
-    dealernet,
-    document
-*/
 /*jslint
     node: true,
     browser: true,
     unparam: true
+    regexp: true
 */
+
+
 'use strict';
+
 var globals = (function (ns) {
-    ns.ERR_LAST_NAME = "Debe introducir tu nombre y al menos un apellido";
+    /*ns.ERR_LAST_NAME = "Debe introducir tu nombre y al menos un apellido";
     ns.ERR_EMAIL = "Debe introducir un correo valido";
     ns.ERR_PASS = "La contraseña debe tener al menos seis caracteres y una " +
         "letra mayúscula, una letra minúscula, un número y un símbolo";
@@ -18,16 +17,19 @@ var globals = (function (ns) {
     ns.ERR_URL = "Debe introducir una URL válida";
     ns.ERR_ADDRESS = "Debe introducir una dirección válida";
     ns.ERR_COUNTRY = "Debe seleccionar un pais válido";
-    ns.ERR_POST_CODE = "Debe introducir un código postal válido";
+    ns.ERR_POST_CODE = "Debe introducir un código postal válido";*/
     ns.COUNTRIES = ["España", "Francia", "Portugal", "Italia", "Alemania"];
     ns.SPAIN = ns.COUNTRIES[0];
     ns.LAST_NAME_REG_EXP = /^[a-zñ\-\'\.]+(?:\s[a-zñ\-\'\.]+)+$/i;
     ns.EMAIL_REG_EXP = /^[\w.]+@[\w.]+\.[\w]{2,6}$/i;
-    ns.PASS_REG_EXP = /^.*(?=.{6,})(?!.*\s)(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\!\@\#\$\%\^\&\*\(\)\-\=\¡\£\_\+\`\~\.\,\<\>\/\?\;\:\'\"\\\|\[\]\{\}]).*$/;
-    ns.URL_REG_EXP = /^[a-z]{2,}:([0-9]+|\/\/){1}[^\s-]*$/i;
+    ns.PASS_REG_EXP = /^.*(?=.{6,})(?!.*\s)(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\!\@\#\$\%\^\&\*\(\)\-\=\¡\£\_\+\`\~\.\,<\>\/\?\;\:\'\"\\\|\[\]\{\}]).*$/;
+    ns.URL_REG_EXP = /^[a-z]{2,}:([0-9]+|\/\/){1}[^\s]*$/i;
     ns.STANDARD_REG_EXP = /^.+$/;
-    ns.POST_CODE_REG_EXP = /^([0-4][0-9]{4}|[5][0-2][0-9]{3})$/;
+    ns.POST_CODE_REG_EXP = /^([1-4][0-9]{4}|5[0-2][0-9]{3}|0[1-9][0-9]{3})$/;
     ns.inputId = ["name", "email", "password", "confirm", "url", "address", "code"];
+    ns.CLASS_RESET = "reset";
+    ns.CLASS_VALID = "valid";
+    ns.CLASS_INVALID = "invalid";
     return ns;
 }({}));
 
@@ -79,58 +81,68 @@ function chooseCountry() {
         form_input.className = "no_display";
     }
 }
-
+function resetForm() {
+    globals.inputId.forEach(function (element) {
+        $(element).value = "";
+        $(element).className = "";
+    });
+    $("comments").value = "";
+    $("select_country").selectedIndex = 0;
+    $("check_buttom").checked = false;
+    $("error").className = "no_display";
+}
 function validateInput(regExp, input) {
     var checkClass;
-    if (input === "") {
-        checkClass = "reset";
+    if (input.value === "") {
+        checkClass = "";
     } else {
-        if (regExp.test(input)) {
-            checkClass = "valid";
+        if (regExp.test(input.value)) {
+            checkClass = globals.CLASS_VALID;
         } else {
-            checkClass = "invalid";
+            checkClass = globals.CLASS_INVALID;
         }
     }
-    return checkClass;
+    input.className = checkClass;
 }
 
 function validateName() {
-    var newClass = validateInput(globals.LAST_NAME_REG_EXP, this.value);
-    this.className = newClass;
+    validateInput(globals.LAST_NAME_REG_EXP, this);
 }
 function validatePass() {
-    var newClass = validateInput(globals.PASS_REG_EXP, this.value);
-    this.className = newClass;
+    validateInput(globals.PASS_REG_EXP, this);
 }
 function validateEmail() {
-    var newClass = validateInput(globals.EMAIL_REG_EXP, this.value);
-    this.className = newClass;
+    validateInput(globals.EMAIL_REG_EXP, this);
 }
 function validateConfirm() {
-    if (this.value === "") {
-        this.className = "reset";
+    var confirm = $("confirm");
+    if (confirm.value === "") {
+        confirm.className = "";
     } else {
-        if (this.value === $("password").value) {
-            this.className = "valid";
+        if (confirm.value === $("password").value) {
+            confirm.className = globals.CLASS_VALID;
         } else {
-            this.className = "invalid";
+            confirm.className = globals.CLASS_INVALID;
         }
     }
 }
 function validateUrl() {
-    var newClass = validateInput(globals.URL_REG_EXP, this.value);
-    this.className = newClass;
+    validateInput(globals.URL_REG_EXP, this);
 }
 function validateStandard() {
-    var newClass = validateInput(globals.STANDARD_REG_EXP, this.value);
-    this.className = newClass;
+    validateInput(globals.STANDARD_REG_EXP, this);
 }
 function validatePostCode() {
-    var newClass = validateInput(globals.POST_CODE_REG_EXP, this.value);
-    this.className = newClass;
+    validateInput(globals.POST_CODE_REG_EXP, this);
 }
+function validateForm() {
+    globals.inputId.forEach(function (element) {
+        $(element).focus();
+    });
+}
+
 function sendForm() {
-    var input; 
+    var input;
     validateForm();
     input = document.getElementsByClassName("invalid");
     if (input.length === 0 && $("check_buttom").checked) {
@@ -139,19 +151,14 @@ function sendForm() {
         $("error").className = "form_input";
     }
 }
-function validateForm() {
-    var inputs;
-    globals.inputId.forEach(function (element) {
-        $(element).focus();
-    });
-}
-window.onload = function () {
-    createError("name_error", globals.ERR_LAST_NAME);
+
+function startApp() {
+    /*createError("name_error", globals.ERR_LAST_NAME);
     createError("email_error", globals.ERR_EMAIL);
     createError("pass_error", globals.ERR_PASS);
     createError("confirm_error", globals.ERR_CONF_PASS);
     createError("url_error", globals.ERR_URL);
-    createError("address_error", globals.ERR_ADDRESS);
+    createError("address_error", globals.ERR_ADDRESS);*/
     buildSelect("select_country", globals.COUNTRIES, "", chooseCountry);
     $("name").addEventListener("keyup", validateName, false);
     $("name").addEventListener("focus", validateName, false);
@@ -168,6 +175,8 @@ window.onload = function () {
     $("code").addEventListener("keyup", validatePostCode, false);
     $("code").addEventListener("focus", validatePostCode, false);
     $("send").addEventListener("click", sendForm, false);
+    resetForm();
     validateForm();
-};
+}
 
+window.onload = startApp;
--- form_validate/html/test/test.calc.js
+++ form_validate/html/test/test.calc.js
@@ -1,28 +1,197 @@
-// Using html2js preprocessor 
+// Using html2js preprocessor
 var HTMLFile = __html__['index.html'],
     body = HTMLFile.split(RegExp("<body>|</body>"))[1];
 
+
 document.body.innerHTML = body;
 
-app();
+
+startApp();
 
 // BDD
 chai.should();
 
-describe('Simple calculator', function() {
-    context('Correct values in operators', function() {
+describe('Form_Validation', function() {
+    context('Validate name field', function() {
         afterEach(function() {
-            $('result').innerHTML = "";
+            resetForm();
         });
-    
-        it('should show 6 for 3 + 3', function() {
-            $('firstoperand').value = 3;
-            $('secondoperand').value = 3;
-            $('add').click();
-            $('result').innerHTML.should.be.equal("6");
+        it('Name should be class valid for name and last_name', function() {
+            $('name').value = "David Villaluenga";
+            validateInput(globals.LAST_NAME_REG_EXP, $("name"));
+            $('name').className.should.be.equal("valid");
+        });
+        it('Name should be class valid for name and last_name', function() {
+            $('name').value = "Steve's Mac-Flanigan J";
+            validateInput(globals.LAST_NAME_REG_EXP, $("name"));
+            $('name').className.should.be.equal("valid");
+        });
+        it('Name should be class empty for empty field', function() {
+            $('name').value = "";
+            validateInput(globals.LAST_NAME_REG_EXP, $("name"));
+            $('name').className.should.be.equal("");
+        });
+        it('Name should be class invalid for only name', function() {
+            $('name').value = "David";
+            validateInput(globals.LAST_NAME_REG_EXP, $("name"));
+            $('name').className.should.be.equal("invalid");
         });
     });
+    context('Validate email field', function() {
+            afterEach(function() {
+                resetForm();
+            });
+            it('Email should be class valid for valid email', function() {
+                $('email').value = "dvillaluenga@gmail.com";
+                validateInput(globals.EMAIL_REG_EXP, $("email"));
+                $('email').className.should.be.equal("valid");
+            });
+            it('Email should be class valid for valid email', function() {
+                $('email').value = "escarate.laravel@gmail.com.es";
+                validateInput(globals.EMAIL_REG_EXP, $("email"));
+                $('email').className.should.be.equal("valid");
+            });
+            it('Email should be class invalid for empty field', function() {
+                $('email').value = "";
+                validateInput(globals.EMAIL_REG_EXP, $("email"));
+                $('email').className.should.be.equal("");
+            });
+            it('Email should be class invalid for invalid email', function() {
+                $('email').value = "dasdas.com";
+                validateInput(globals.LAST_NAME_REG_EXP, $("email"));
+                $('email').className.should.be.equal("invalid");
+            });
+    });
+    context('Validate password field', function() {
+            afterEach(function() {
+                resetForm();
+            });
+            it('Password field should be class valid for valid password', function() {
+                $('password').value = "aA1!aaaaa/$%&/()=?<>^*_:";
+                validateInput(globals.PASS_REG_EXP, $("password"));
+                $('password').className.should.be.equal("valid");
+            });
+            it('Password field should be class valid for valid password', function() {
+                $('password').value = "1LP#aa";
+                validateInput(globals.PASS_REG_EXP, $("password"));
+                $('password').className.should.be.equal("valid");
+            });
+            it('Password field be class empty for empty field', function() {
+                $('password').value = "";
+                validateInput(globals.PASS_REG_EXP, $("password"));
+                $('password').className.should.be.equal("");
+            });
+            it('Password field be class invalid for short password', function() {
+                $('password').value = "aA!1W";
+                validateInput(globals.PASS_REG_EXP, $("password"));
+                $('password').className.should.be.equal("invalid");
+            });
+            it('Password field be class invalid for no number in password', function() {
+                $('password').value = "a!AdA$";
+                validateInput(globals.PASS_REG_EXP, $("password"));
+                $('password').className.should.be.equal("invalid");
+            });
+            it('Password should be class invalid for password with no simbols', function() {
+                $('password').value = "aA21Aa";
+                validateInput(globals.PASS_REG_EXP, $("password"));
+                $('password').className.should.be.equal("invalid");
+            });
+    });
+    context('Validate confirm_password field', function() {
+            afterEach(function() {
+                resetForm();
+            });
+            it('Confirm_password field should be class valid for equal field password', function() {
+                $('password').value = "aA1";
+                $('confirm').value = "aA1";
+                validateConfirm();
+                $('confirm').className.should.be.equal("valid");
+            });
+            it('Confirm_password field should be class empty for empty field password and confirm_password', function() {
+                $('password').value = "";
+                $('confirm').value = "";
+                validateConfirm();
+                $('confirm').className.should.be.equal("");
+            });
+            it('Confirm_password field should be class invalid for different field password and confirm_password', function() {
+                $('password').value = "aA";
+                $('confirm').value = "aA1";
+                validateConfirm();
+                $('confirm').className.should.be.equal("invalid");
+            });
+    });
+    context('Validate url field', function() {
+            afterEach(function() {
+                resetForm();
+            });
+            it('URL field should be class valid for local url', function() {
+                $('url').value = "localhost:8000/";
+                validateInput(globals.URL_REG_EXP, $("url"));
+                $('url').className.should.be.equal("valid");
+            });
+            it('URL field should be class valid for external url', function() {
+                $('url').value = "http://www.aula-daw.com/";
+                validateInput(globals.URL_REG_EXP, $("url"));
+                $('url').className.should.be.equal("valid");
+            });
+            it('URL field should be class empty for empty url field', function() {
+                $('url').value = "";
+                validateInput(globals.URL_REG_EXP, $("url"));
+                $('url').className.should.be.equal("");
+            });
+            it('URL field should be class invalid for local url with white spaces', function() {
+                $('url').value = "localhost:800 /";
+                validateInput(globals.URL_REG_EXP, $("url"));
+                $('url').className.should.be.equal("invalid");
+            });
+            it('URL field should be class invalid for invalid url', function() {
+                $('url').value = "pepe";
+                validateInput(globals.URL_REG_EXP, $("url"));
+                $('url').className.should.be.equal("invalid");
+            });
+            it('URL field should be class invalid for invalid url', function() {
+                $('url').value = "htp//hola.com";
+                validateInput(globals.URL_REG_EXP, $("url"));
+                $('url').className.should.be.equal("invalid");
+            });
+    });
+    context('Validate post-code field', function() {
+            afterEach(function() {
+                resetForm();
+            });
+            it('Post-code field should be class valid for postal code 52999', function() {
 
+                $("code").value = "52999";
+                validateInput(globals.POST_CODE_REG_EXP, $("code"));
+                $('code').className.should.be.equal("valid");
+            });
+            it('Post-code field should be class valid for postal code 01000', function() {
+                $('code').value = "01000";
+                validateInput(globals.POST_CODE_REG_EXP, $("code"));
+                $('code').className.should.be.equal("valid");
+            });
+            it('Post-code field should be class empty for empty postal code', function() {
+                $("code").value = "";
+                validateInput(globals.POST_CODE_REG_EXP, $("code"));
+                $('code').className.should.be.equal("");
+            });
+            it('Post-code field should be class invalid for postal code 0000A', function() {
+                $('code').value = "0000A";
+                validateInput(globals.POST_CODE_REG_EXP, $("code"));
+                $('code').className.should.be.equal("invalid");
+            });
+            it('Post-code field should be class invalid for postal code 1111', function() {
+                $('code').value = "1111";
+                validateInput(globals.POST_CODE_REG_EXP, $("code"));
+                $('code').className.should.be.equal("invalid");
+            });
+            it('Post-code field should be class invalid for postal code 00000', function() {
+                $('code').value = "00000";
+                validateInput(globals.POST_CODE_REG_EXP, $("code"));
+                $('code').className.should.be.equal("invalid");
+            });
+    });
+/*
     context('Incorrect values in operators', function() {
         afterEach(function() {
             $('result').innerHTML = "";
@@ -34,5 +203,5 @@ describe('Simple calculator', function() {
             $('add').click();
             $('result').innerHTML.should.be.equal(ERROR_MSG);
         });
-    });
+    });*/
 });
